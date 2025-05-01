83c8d4bf071538f7dc080a21b974ed853a06712d
Akem-Ben <akemini.ndaobong@gmail.com>
2025-05-01 15:34:54 +0100

    Bug fixes

--- waiting-list-service/dist/config/db.d.ts
+++ waiting-list-service/dist/config/db.d.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from 'sequelize';
+export declare const config: {
+    dbUrl: string | undefined;
+};
 declare const sequelize: Sequelize;
 export declare const connectDB: () => Promise<void>;
 export default sequelize;
