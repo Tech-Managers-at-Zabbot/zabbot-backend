682c3cfd389d030a5e3e9b0675bb8ac52b2468ec
Akem-Ben <akemini.ndaobong@gmail.com>
2025-05-18 22:38:57 +0100

    Fixes

--- notification-service/dist/services/sendgridServices.js
+++ notification-service/dist/services/sendgridServices.js
@@ -6,10 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const utilities_1 = require("../../../shared/utilities");
 const sendgridConfig_1 = __importDefault(require("../config/sendgridConfig"));
 const sendWelcomeFoundingListEmailService = utilities_1.errorUtilities.withServiceErrorHandling(async (email, firstName, lastName) => {
-    console.log('Sending email to:', email, firstName, lastName);
     const token = utilities_1.helpersUtilities.generateToken({ email }, '30d');
-    const unsubscribeUrl = `${process.env.PRODUCTION_FRONTEND_URL}/founders-circle/unsubscribe?${token}`;
-    console.log('Unsubscribe URL:', unsubscribeUrl);
+    const unsubscribeUrl = `${process.env.UNSUBSCRIBE_URL}/founders-circle/unsubscribe?token=${token}`;
     const messageDetails = {
         to: email,
         from: {
@@ -72,37 +70,63 @@ const addToSendGridFoundersList = utilities_1.errorUtilities.withServiceErrorHan
         throw error;
     }
 });
-const removeFromFoundersListService = utilities_1.errorUtilities.withServiceErrorHandling(async (email) => {
-    // 1. Remove from founders list
-    const deleteResponse = await sendgridConfig_1.default.sendgridClient.request({
-        method: 'DELETE',
-        url: `/v3/marketing/lists/${process.env.SENDGRID_FOUNDERS_LIST_ID}/contacts`,
-        body: {
-            contact_ids: [email]
-        },
-    });
-    // Successful removal returns 202 or 204
-    if (deleteResponse[0].statusCode !== 202 && deleteResponse[0].statusCode !== 204) {
-        throw utilities_1.errorUtilities.createError('Process failed, please try again later', deleteResponse[0].statusCode);
+const removeFromSengridFoundersListService = utilities_1.errorUtilities.withServiceErrorHandling(async (email) => {
+    try {
+        const data = {
+            emails: [email],
+        };
+        const [findContact] = await sendgridConfig_1.default.sendgridClient.request({
+            method: "POST",
+            url: `/v3/marketing/contacts/search/emails`,
+            body: data,
+        });
+        if (findContact?.statusCode !== 200) {
+            throw utilities_1.errorUtilities.createError('Process failed, please try again later', findContact.statusCode);
+        }
+        const sendgridUserId = findContact?.body?.result[email]?.contact?.id;
+        if (!sendgridUserId) {
+            throw utilities_1.errorUtilities.createError('User not found', 404);
+        }
+        const userFirstName = findContact?.body?.result[email]?.contact?.first_name;
+        const userLastName = findContact?.body?.result[email]?.contact?.last_name;
+        const queryParams = { contact_ids: sendgridUserId };
+        // 1. Remove from founders list
+        const deleteResponse = await sendgridConfig_1.default.sendgridClient.request({
+            method: 'DELETE',
+            url: `/v3/marketing/lists/${process.env.SENDGRID_FOUNDERS_LIST_ID}/contacts`,
+            qs: queryParams,
+        });
+        // Successful removal returns 202 or 204
+        if (deleteResponse[0].statusCode !== 202 && deleteResponse[0].statusCode !== 204) {
+            throw utilities_1.errorUtilities.createError('Process failed, please try again later', deleteResponse[0].statusCode);
+        }
+        const unsubscribeData = {
+            contacts: [
+                {
+                    email,
+                    first_name: userFirstName,
+                    last_name: userLastName,
+                },
+            ],
+            list_ids: [process.env.SENDGRID_FOUNDERS_LIST_ID],
+        };
+        const addResponse = await sendgridConfig_1.default.sendgridClient.request({
+            method: 'PUT',
+            url: `/v3/marketing/contacts`,
+            body: unsubscribeData
+        });
+        if (addResponse[0].statusCode !== 202) {
+            throw utilities_1.errorUtilities.createError('Process failed, please try again later', addResponse[0].statusCode);
+        }
+        return utilities_1.responseUtilities.handleServicesResponse(200, 'Successfully processed unsubscription');
     }
-    // 2. Add to unsubscribe list
-    const addResponse = await sendgridConfig_1.default.sendgridClient.request({
-        method: 'PUT',
-        url: `/v3/marketing/contacts`,
-        body: {
-            contacts: [{ email }],
-            list_ids: [process.env.SENDGRID_UNSUBSCRIBE_GROUP_ID]
-        },
-    });
-    // Successful addition returns 202
-    if (addResponse[0].statusCode !== 202) {
-        throw utilities_1.errorUtilities.createError('Process failed, please try again later', addResponse[0].statusCode);
+    catch (error) {
+        console.error('Error Unsubscribing:', error.response?.body || error);
+        throw utilities_1.errorUtilities.createError(`Error Unsubscribing: ${error.response?.body || error}`, 500);
     }
-    console.log(`Successfully processed unsubscription for ${email}`);
-    return utilities_1.responseUtilities.handleServicesResponse(200, 'Successfully processed unsubscription');
 });
 exports.default = {
     sendWelcomeFoundingListEmailService,
     addToSendGridFoundersList,
-    removeFromFoundersListService
+    removeFromSengridFoundersListService
 };
--- waiting-list-service/dist/controllers/waitingListController.js
+++ waiting-list-service/dist/controllers/waitingListController.js
@@ -3,12 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.joinWaitingList = exports.config = void 0;
+exports.unsubscribeWaitingList = exports.joinWaitingList = exports.config = void 0;
 const waitingList_1 = __importDefault(require("../entities/waitingList"));
 const uuid_1 = require("uuid");
 const axios_1 = __importDefault(require("axios"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
+const utilities_1 = require("../../../shared/utilities");
 dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../../../.env') });
 exports.config = {
     NOTIFICATION_SERVICE_ROUTE: process.env.NOTIFICATION_SERVICE_ROUTE
@@ -62,3 +63,45 @@ const joinWaitingList = async (request, response) => {
     }
 };
 exports.joinWaitingList = joinWaitingList;
+const unsubscribeWaitingList = async (request, response) => {
+    try {
+        const { token } = request.query;
+        if (!token) {
+            throw utilities_1.errorUtilities.createError('Token is required', 400);
+        }
+        const decodedDetails = utilities_1.helpersUtilities.validateToken(token);
+        if (!decodedDetails) {
+            console.error('Invalid token');
+            throw utilities_1.errorUtilities.createError('Error, please try again', 400);
+        }
+        const { email } = decodedDetails.data;
+        if (!email) {
+            throw utilities_1.errorUtilities.createError('Error, please try again', 400);
+        }
+        const existingUser = await waitingList_1.default.findOne({ where: { email } });
+        if (!existingUser) {
+            return response.status(404).json({ message: 'Email not found in founders list' });
+        }
+        const sendgridResponse = await axios_1.default.post(`${exports.config.NOTIFICATION_SERVICE_ROUTE}/founding-list/unsubscribe`, { email });
+        if (sendgridResponse.status !== 200) {
+            console.error('Failed to unsubscribe from SendGrid:', sendgridResponse.data, sendgridResponse.status, sendgridResponse);
+            return response.status(500).json({ message: 'Failed to unsubscribe from SendGrid' });
+        }
+        await waitingList_1.default.destroy({ where: { email } });
+        return response.status(200).json({
+            message: 'Successfully unsubscribed from founders list'
+        });
+    }
+    catch (error) {
+        console.error(error);
+        if (error.name === 'SequelizeValidationError') {
+            response.status(400).json({
+                message: 'Validation error',
+                errors: error.errors.map((err) => err.message)
+            });
+            return;
+        }
+        response.status(500).json({ message: 'Server error' });
+    }
+};
+exports.unsubscribeWaitingList = unsubscribeWaitingList;
