5113aca4855aff9602f94f2364c863687698738e
Akem-Ben <akemini.ndaobong@gmail.com>
2025-06-18 01:40:31 +0100

    Bug fixes

--- notification-service/dist/shared/utilities/index.d.ts
+++
@@ -1,3 +0,0 @@
-import responseUtilities from "./responseHandlers/response.utilities";
-import errorUtilities from './errorHandlers/errorHandlers.utilities';
-export { responseUtilities, errorUtilities, };
