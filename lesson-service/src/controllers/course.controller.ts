40f2069828d4ac2009d51edac7d771b24b2cd274
Monday Udosen <henro450@gmail.com>
2025-07-24 20:15:45 +0100

    Feat: Implement course and user course management controllers and services

---
+++ lesson-service/src/controllers/course.controller.ts
@@ -0,0 +1,93 @@
+import { Request, Response } from 'express';
+import courseService from '../services/lessonServices/course.service';
+import userCourseService from '../services/lessonServices/user-course.service';
+import { errorUtilities, responseUtilities } from '../../../shared/utilities';
+
+
+// Controller to get all courses
+export const getCoursesController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+      const { isActive } = req.query;
+      const courses = await courseService.getCourses(req.query);
+      return responseUtilities.responseHandler(res, courses.message, courses.statusCode, courses.data);
+  }
+);
+
+// Controller to get a single course
+export const getCourseController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const course = await courseService.getCourse(id);
+    return responseUtilities.responseHandler(res, course.message, course.statusCode, course.data);
+  }
+);
+
+// Controller to get a course by title
+export const getCourseByTitleController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const { title } = req.params;
+    const course = await courseService.getCourseByTitle(title);
+    return responseUtilities.responseHandler(res, course.message, course.statusCode, course.data);
+  }
+);
+
+// Controller to create a new course
+export const addCourseController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const payload = req.body;
+    const course = await courseService.addCourse(payload);
+    return responseUtilities.responseHandler(res, course.message, course.statusCode, course.data);
+  }
+);
+
+// Controller to update an existing course
+export const updateCourseController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const payload = req.body;
+    const course = await courseService.updateCourse(id, payload);
+    return responseUtilities.responseHandler(res, course.message, course.statusCode, course.data);
+  }
+);
+
+// Controller to delete a course
+export const deleteCourseController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const course = await courseService.deleteCourse(id);
+    return responseUtilities.responseHandler(res, course.message, course.statusCode, course.data);
+  }
+);
+
+// Controller to get all user courses
+export const getUserCoursesController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const userCourses = await userCourseService.getUserCourses(req.query);
+    return responseUtilities.responseHandler(res, userCourses.message, userCourses.statusCode, userCourses.data);
+  }
+);
+
+// Controller to get user course
+export const getUserCourseController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const userCourse = await userCourseService.getUserCourse(req.params);
+    return responseUtilities.responseHandler(res, userCourse.message, userCourse.statusCode, userCourse.data);
+  }
+);
+
+// Controller to add user to course
+export const addUserCourseController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const addUserCourse = await userCourseService.addUserCourse(req.body);
+    return responseUtilities.responseHandler(res, addUserCourse.message, addUserCourse.statusCode, addUserCourse.data);
+  }
+);
+
+// Controller to update user course
+export const updateUserCourseController = errorUtilities.withControllerErrorHandling(
+  async (req: Request, res: Response) => {
+    const {id, userCourseData} = req.body;
+    const updateUserCourse = await userCourseService.updateUserCourse({id, userCourseData});
+    return responseUtilities.responseHandler(res, updateUserCourse.message, updateUserCourse.statusCode, updateUserCourse.data);
+  }
+);
---
+++ lesson-service/src/routes/course.route.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+import {
+    getCoursesController,
+    getCourseController,
+    getCourseByTitleController,
+    addCourseController,
+    updateCourseController,
+    getUserCoursesController,
+    getUserCourseController,
+    addUserCourseController,
+    updateUserCourseController
+} from '../controllers/course.controller';
+
+const router = express.Router();
+
+router.get('/', getCoursesController);
+router.get('/:id', getCourseController);
+router.get('/title/:title', getCourseByTitleController);
+router.post('/', addCourseController);
+router.put('/', updateCourseController);
+
+router.get('/user', getUserCoursesController);
+router.get('/user/:id', getUserCourseController);
+router.post('/user', addUserCourseController);
+router.put('/user', updateUserCourseController)
+
+
+export default router;
