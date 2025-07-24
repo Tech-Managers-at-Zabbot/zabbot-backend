87e9c4f1d1b763f4885adc1672bed527069abe85
Monday Udosen <henro450@gmail.com>
2025-07-24 21:12:25 +0100

    Empty commit with no code changes made.

--- lesson-service/src/routes/index.ts
+++ lesson-service/src/routes/index.ts
@@ -3,12 +3,14 @@ import lessonRouter from './lesson.route';
 import languageRouter from './language.route';
 import dailyWordRouter from './dailyWord.route';
 import contentRouter from './content.route';
+import courseRouter from './course.route';
 
 const rootRouter = express.Router()
 
 rootRouter.use('/lessons', lessonRouter);
 rootRouter.use('/languages', languageRouter);
-rootRouter.use('/daily-words', dailyWordRouter)
+rootRouter.use('/daily-words', dailyWordRouter);
 rootRouter.use('/contents', contentRouter);
+rootRouter.use('/courses', courseRouter);
 
 export default rootRouter;
