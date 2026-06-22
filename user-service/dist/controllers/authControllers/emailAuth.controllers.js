261cc08082b9b49e3387e1452838b3a2f76fc4b3
oagboola <lydexmail@yahoo.com>
2026-06-23 00:01:18 +0100

    update

--- user-service/dist/controllers/authControllers/emailAuth.controllers.js
+++ user-service/dist/controllers/authControllers/emailAuth.controllers.js
@@ -1,9 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const services_1 = require("../../services");
 const utilities_1 = require("../../../../shared/utilities");
+const general_responses_1 = require("../../responses/generalResponses/general.responses");
 const statusCodes_responses_1 = require("../../../../shared/statusCodes/statusCodes.responses");
 const otp_responses_1 = require("../../responses/otpResponses/otp.responses");
+const axios_1 = __importDefault(require("axios"));
+const config_1 = __importDefault(require("../../../../config/config"));
 /**
  * @description Controller for user registration
  * @param request - Express Request object
@@ -13,6 +19,38 @@ const otp_responses_1 = require("../../responses/otpResponses/otp.responses");
 // user-service registration controller
 const userRegistrationController = utilities_1.errorUtilities.withControllerErrorHandling(async (request, response) => {
     const payloadDetails = request.body;
+    const { email } = payloadDetails;
+    try {
+        const isBetaTester = await axios_1.default.get(`${config_1.default.LOCAL_FOUNDERS_LIST_URL}/beta-tester-check?email=${email}`, {
+            timeout: 10000,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (isBetaTester.status !== statusCodes_responses_1.StatusCodes.OK) {
+            const errorMessage = isBetaTester.status === statusCodes_responses_1.StatusCodes.Forbidden
+                ? general_responses_1.GeneralResponses.UNAUTHORIZED_FOR_TESTING
+                : isBetaTester.data.message || general_responses_1.GeneralResponses.FAILED_TESTER_CHECK;
+            throw utilities_1.errorUtilities.createError(errorMessage, isBetaTester.status);
+        }
+    }
+    catch (error) {
+        console.log("📊 Error details:", {
+            status: error.response?.status,
+            statusText: error.response?.statusText,
+            data: error.response?.data,
+            code: error.code,
+        });
+        if (error.response?.status === statusCodes_responses_1.StatusCodes.NotFound) {
+            throw utilities_1.errorUtilities.createError(general_responses_1.GeneralResponses.SIGNUP_AS_TESTER, statusCodes_responses_1.StatusCodes.NotFound);
+        }
+        else if (error.response?.status === 403) {
+            throw utilities_1.errorUtilities.createError(general_responses_1.GeneralResponses.UNAUTHORIZED_FOR_TESTING, statusCodes_responses_1.StatusCodes.Forbidden);
+        }
+        else {
+            throw utilities_1.errorUtilities.createError(general_responses_1.GeneralResponses.FAILED_TESTER_CHECK, statusCodes_responses_1.StatusCodes.InternalServerError);
+        }
+    }
     const registerUser = await services_1.emailAuthServices.registerUserService(payloadDetails);
     return utilities_1.responseUtilities.responseHandler(response, registerUser.message, registerUser.statusCode, registerUser.data);
 });
